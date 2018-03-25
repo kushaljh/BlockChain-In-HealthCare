040cb99aaf6b5ffcbcc7bccec343e341c70be9df
Kushal Jhunjhunwalla <kushal.jhunjhunwalla@gmail.com>
2018-03-24 20:45:05 -0700

    fix get and post request scripts

--- putScript.py
+++ putScript.py
@@ -2,29 +2,25 @@ import requests
 import json
 
 url = "https://api.tierion.com/v1/datastores"
-payload = {'X-Username': 'kushal.jhunjhunwalla@gmail.com', 'X-Api-Key' : 'QKGs+DurASnHUPuR21MyiONYYR3XAtOKtOf04xYzUIA=', 
-            'Content-Type': 'application/x-www-form-urlencoded'}
+payload = {'X-Username': 'kushal.jhunjhunwalla@gmail.com', 'X-Api-Key' : 'QKGs+DurASnHUPuR21MyiONYYR3XAtOKtOf04xYzUIA=',
+            'Content-Type': 'application/json'}
 
 def newUser():
     data = {}
     data['datastoreId'] = 7132;
     data['name'] = input("Enter name : ");
     data['groupName'] = input("Enter groupName : ");
-    #redirectEnabled = input("Enter redirectEnabled : ")
-    #redirectUrl = input("Enter redirectUrl : ")
-    #emailNotificationEnabled = input("Enter emailNotificationEnabled : ")
-    #emailNotificationAddress = input("Enter emailNotificationAddress : ")
-    #postDataEnabled = input("Enter postDataEnabled : ")
-    #postDataUrl = input("Enter postDataUrl : ")
-    #postReceiptEnabled = input("Enter postReceiptEnabled : ")
-    #postReceiptUrl = input("Enter postReceiptUrl : ")
     data_json = json.dumps(data)
-    print(data_json)
+    #print(data_json)
     return data_json
 
 add_url = "https://api.tierion.com/v1/records"
 
-response = requests.post(add_url, json = newUser(), headers = payload, params = {'datastoreId' : 7132});
+dataItem = newUser()
 
-print(response.status_code)
-print(response.json())
+#print("test", dataItem)
+
+response = requests.post(add_url, data = dataItem, headers = payload);
+
+#print(response.status_code)
+#print(response.json())
--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 import requests
+import json
 
 url = "https://api.tierion.com/v1/datastores"
-payload = {'X-Username': 'kushal.jhunjhunwalla@gmail.com', 'X-Api-Key' : 'QKGs+DurASnHUPuR21MyiONYYR3XAtOKtOf04xYzUIA=', 'Content-Type': 'application/json'}
+payload = {'X-Username': 'kushal.jhunjhunwalla@gmail.com',
+            'X-Api-Key' : 'QKGs+DurASnHUPuR21MyiONYYR3XAtOKtOf04xYzUIA=',
+            'Content-Type': 'application/json'}
 
 
 response = requests.get(url, headers = payload)
 
 print(response.status_code)
 print(response.content)
+
+record_url = "https://api.tierion.com/v1/records?datastoreId=7132"
+response = requests.get(record_url, headers = payload)
+data = response.content #json.load(response.content, indent = 4)
+
+
+print("get : ", response.status_code)
+print("get : ", data)
