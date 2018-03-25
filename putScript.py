e77b1b9c14c7ff3988c81af99c9771d20be75cea
Kushal Jhunjhunwalla <kushal.jhunjhunwalla@gmail.com>
2018-03-24 22:20:56 -0700

    Update scripts to work according to project

--- putScript.py
+++ putScript.py
@@ -1,26 +1,22 @@
 import requests
 import json
 
-url = "https://api.tierion.com/v1/datastores"
 payload = {'X-Username': 'kushal.jhunjhunwalla@gmail.com', 'X-Api-Key' : 'QKGs+DurASnHUPuR21MyiONYYR3XAtOKtOf04xYzUIA=',
             'Content-Type': 'application/json'}
 
 def newUser():
     data = {}
     data['datastoreId'] = 7132;
-    data['name'] = input("Enter name : ");
-    data['groupName'] = input("Enter groupName : ");
+    data['Full Name'] = input("Enter name: ");
+    data['Email'] = input("Enter email: ");
+    data['Phone Number'] = input("Enter phone number: ");
+    data['Prescription'] = input("Enter drug name: ");
+    data['Dosage'] = input("Enter dosage: ");
     data_json = json.dumps(data)
-    #print(data_json)
     return data_json
 
 add_url = "https://api.tierion.com/v1/records"
 
 dataItem = newUser()
 
-#print("test", dataItem)
-
 response = requests.post(add_url, data = dataItem, headers = payload);
-
-#print(response.status_code)
-#print(response.json())
--- test.py
+++ test.py
@@ -14,7 +14,8 @@ print(response.content)
 
 record_url = "https://api.tierion.com/v1/records?datastoreId=7132"
 response = requests.get(record_url, headers = payload)
-data = response.content #json.load(response.content, indent = 4)
+data = response.content #
+#data1 = json.loads(response.content, indent=4,separators=(',', ':'))
 
 
 print("get : ", response.status_code)
